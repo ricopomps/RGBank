97eeef3615991d3633542efd5c433528df75e64b
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2022-08-19 19:16:46 -0300

    feat: Basic configs and connection to db

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+import Logging from './library/Logging';
+
+const router = express();
+
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('Connected to database');
+        StartServer();
+    })
+    .catch((error) => Logging.error('Error while connecting to database: ' + error));
+
+const StartServer = () => {
+    router.use((req, res, next) => {
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`));
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+
+    router.use(express.json());
+
+    router.use((req, res, next) => {
+        res.header('Acess-Control-Allow-Origin', '*');
+        res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
+
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server running in port ${config.server.port}`));
+};
